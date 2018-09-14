b75ddfcbddefdaca283689810871486feb05eabc
steven libonati <slibonat-redhat.com@bastion.95f7.example.opentlc.com>
2018-09-14 22:44:04 +0000

    initial commit

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -29,3 +29,22 @@ echo "Setting up Nexus in project $GUID-nexus"
 # oc new-app -f ../templates/nexus.yaml --param .....
 
 # To be Implemented by Student
+
+
+## oc new-app -f ../templates/nexus.yaml
+
+# while : ; do
+#   echo "Checking if Nexus is Ready..."
+#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
+#   [[ "$?" == "1" ]] || break
+#   echo "...no. Sleeping 10 seconds."
+#   sleep 10
+# done
+
+## curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+## chmod +x setup_nexus3.sh
+## ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+## rm setup_nexus3.sh
+
+## oc expose dc nexus3 --port=5000 --name=nexus-registry
+## oc create route edge nexus-registry --service=nexus-registry --port=5000
