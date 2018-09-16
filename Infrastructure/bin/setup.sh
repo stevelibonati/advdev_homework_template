89a45d4146a3d6515efd12cd4a6940e23d419342
steven libonati <slibonat-redhat.com@bastion.95f7.example.opentlc.com>
2018-09-16 21:04:58 +0000

    added sonarqube

---
+++ Infrastructure/bin/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./setup_projects.sh sjl slibonat-redhat.com
+./setup_sonar.sh sjl
+./setup_jenkins.sh sjl https://github.com/slibonati/advdev_homework_template na39.openshift.opentlc.com
+./setup_nexus.sh sjl
+
--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,5 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+
+oc new-app -f ../templates/sjl-sonarqube.yaml -n sjl-sonarqube
