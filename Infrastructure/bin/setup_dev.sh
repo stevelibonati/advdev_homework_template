0f7139d90e8f2d27297c7b45081beb3213b88f22
steven libonati <slibonat-redhat.com@bastion.95f7.example.opentlc.com>
2018-09-16 21:21:51 +0000

    added jenkins permission

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,3 +12,7 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
+oc get -o=yaml template mongodb-persistent -n openshift | oc process -f - -p MONGODB_USER=mongodb -p MONGODB_PASSWORD=mongodb -p MONGODB_DATABASE=parks -p MONGODB_ADMIN_PASSWORD=mongodb -p VOLUME_CAPACITY=2Gi | oc create -f - -n ${GUID}-parks-dev
+
+oc policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n ${GUID}-parks-dev
+#oc policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n ${GUID}-parks-prod
